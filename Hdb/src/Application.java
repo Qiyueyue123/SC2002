cbca89ebacd27fd67b62718c34c22c7e2aabcc71
Joshua Lee <151831403+Joshhh0908@users.noreply.github.com>
2025-03-30 04:10:57 +0800

    Added bunch of applicant methods and the respective functions into applicantdisplay
    havent yet added the two extended functions of view applied project

--- Hdb/src/Application.java
+++ Hdb/src/Application.java
@@ -1,3 +1,28 @@
 public class Application {
+    Project appliedProject;
+	String appliedStatus;
+    int flatType;
+
+    public Application() {
+        appliedStatus = "Unapplied";
+    }
+
+    public void setAppliedProject(Project p) {
+        appliedProject = p;
+    }
+    public void setAppliedStatus(String s) {
+        appliedStatus = s;
+    }
+    public String getAppliedStatus() {
+        return appliedStatus;
+    }
+    public void setFlatType(int r) {
+        flatType = r;
+    }
+    public void print() {
+        System.out.println("Project: " + appliedProject.getName());
+        System.out.println("Application Status: " + appliedStatus);
+        System.out.println("FlatType: " + flatType);
+    }
     
 }
--- Hdb/src/TestingApp.java
+++ Hdb/src/TestingApp.java
@@ -2,17 +2,20 @@
 // this is NOT meant to replace the actual main app
 // hence it does not have any of the log in functions 
 
+import java.util.ArrayList;
+
 public class TestingApp {
     public static void main(String[] args) {
         new EnquiryList(); //singular enquiry list created to handle projects, bc obj need to be created b4 can be used
 		new ProjectList(); //singular project list created to handle projects, bc obj need to be craeted b4 can be used
+		Application a = new Application();
+		ArrayList<Applicant> dummyList = new ArrayList<Applicant>();
+		Applicant Joe = new Applicant("Joe", 40, true, a);
+		Applicant Tim = new Applicant("Tim", 23, true, a);
 		
-		Applicant Joe = new Applicant("Joe");
-		Applicant Tim = new Applicant("Tim");
-		
-		Project AllGroves = new Project("All  Groves");
-		Project ZedWays = new Project("ZedWays");
-		Project BummerGroves = new Project("BummerGroves");
+		Project AllGroves = new Project("All  Groves", true, 3, 4, dummyList);
+		Project ZedWays = new Project("ZedWays", true, 0, 5, dummyList);
+		Project BummerGroves = new Project("BummerGroves", true, 12, 0, dummyList);
 		
 		ProjectList.addProject(ZedWays);
 		ProjectList.addProject(AllGroves);
