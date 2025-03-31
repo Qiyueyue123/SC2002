703434c0b7bda850e45f6d42a4007244a2bc0f40
Qiyueyue123 <qli041@e.ntu.edu.sg>
2025-03-31 19:46:19 +0800

    added user basics and edited applicant to use super

--- Hdb/src/User.java
+++ Hdb/src/User.java
@@ -1,3 +1,34 @@
 public abstract class User {
     
+    protected String name;
+    protected int age;
+    protected boolean married;
+    protected String password;
+    protected String nric;
+
+    public User(String name, int age, boolean married, String nric) {
+        this.name = name;
+        this.age = age;
+        this.married = married;
+        this.nric = nric;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getNRIC(){
+        return nric;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public boolean isMarried(){
+        return married;
+    }
+
+    
+
 }
